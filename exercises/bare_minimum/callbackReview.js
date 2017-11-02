09176f9d4491eeb1bb3bf7ba4491443d93eee8c3
ephraimg <ephraim.glick@gmail.com>
2017-11-02 14:38:51 -0700

    Complete callbackReview

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -10,34 +10,22 @@ var request = require('request');
 var pluckFirstLineFromFile = function (filePath, callback) {
   fs.readFile(filePath, (err, data) => {
     if (err) { 
-      callback(err, null); 
-    } else { 
-      callback(null, data.toString().split('\n')[0]); 
+      callback(err); 
+    } else {
+      callback(null, data.toString().split('\n')[0]);
     }
   });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url, callback) {
-  // https.get(url, res => {
-  // //   res.on('data', data => callback(null, res.statusCode));
-  // // }).on('error', err => callback(err));
-  //   res.on('data', callback(null, res.statusCode));
-  // }).on('error', callback(err));
- 
-  var req = https.request(url, (res) => {
-    console.log('statusCode:', res.statusCode);
-    callback(null, res.statusCode);
-    console.log('headers:', res.headers);
+  request(url, (err, res, body) => {
+    if (err) { 
+      callback(err); 
+    } else {
+      callback(null, res.statusCode);
+    }
   });
-  req.on('error', (e) => {
-    console.log('message err: ', e.message);
-    callback(e, null);
-  });
-  req.end();
-
-
- 
 };
 
 // Export these functions so we can test them and reuse them in later exercises
